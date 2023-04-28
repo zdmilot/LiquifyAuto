5b6dcab4e54ae295698358cf5682ead9e47aeb8c
Zachary Milot <zdmilot@gmail.com>
2023-04-28 13:31:14 -0400

    Changed comments in authenticated app

--- liquifai/flask_app.py
+++ liquifai/flask_app.py
@@ -1,22 +1,15 @@
+# Import necessary libraries
 import uuid
 import requests
 from flask import Flask, render_template, session, request, redirect, url_for
 from flask_session import Session  # https://pythonhosted.org/Flask-Session
 import msal
 import app_config
-import logging
 
 
 app = Flask(__name__)
 app.config.from_object(app_config)
 Session(app)
-
-# This section is needed for url_for("foo", _external=True) to automatically
-# generate http scheme when this sample is running on localhost,
-# and to generate https scheme when it is deployed behind reversed proxy.
-# See also https://flask.palletsprojects.com/en/1.0.x/deploying/wsgi-standalone/#proxy-setups
-#from werkzeug.middleware.proxy_fix import ProxyFix
-#app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1, x_host=1)
 
 @app.route("/")
 def index():
@@ -26,8 +19,7 @@ def index():
 
 @app.route("/login")
 def login():
-    # Technically we could use empty list [] as scopes to do just sign in,
-    # here we choose to also collect end user consent upfront
+    # Collect end user consent upfront
     session["flow"] = _build_auth_code_flow(scopes=app_config.SCOPE)
     return render_template("login.html", auth_url=session["flow"]["auth_uri"], version=msal.__version__)
 
