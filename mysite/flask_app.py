496eec16c0b2e89cb1da85a1bb22109199c00f99
Zachary Milot <zdmilot@gmail.com>
2023-04-28 02:02:25 +0000

    Added proper .env variables, added functionality for word and txt file processing and optimized openAI prompt

--- mysite/flask_app.py
+++ mysite/flask_app.py
@@ -3,6 +3,9 @@ from flask import Flask, render_template, request, Response, redirect, session, 
 import pandas as pd
 import openai
 import os
+import io
+from docx import Document
+
 
 # Initialize Flask app and OpenAI API key
 app = Flask(__name__)
@@ -13,13 +16,33 @@ app.secret_key = os.urandom(24)  # Add a secret key for the session
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
+         # Check if a file is uploaded
+        if 'file' not in request.files:
+            error_message = "No file uploaded. Please select a file and try again."
+            return render_template('index.html', error=error_message)
+
         # Get input data
-        excel_file = request.files['file']
+        input_file = request.files['file']
+        if not input_file.filename:  # Check if the filename is empty (i.e., no file selected)
+            error_message = "No file selected. Please select a file and try again."
+            return render_template('index.html', error=error_message)
+        file_extension = input_file.filename.split('.')[-1]
         output_format = request.form.get('output_format', 'table')
 
-        # Process input data
-        df = pd.read_excel(excel_file, engine='openpyxl')
-        processed_data = process_data(df)
+        # Process input data based on file type
+        if file_extension == 'xlsx':
+            df = pd.read_excel(input_file, engine='openpyxl')
+            lines = [f"{row[df.columns[0]]}{row[df.columns[1]]}{row[df.columns[2]]}" for _, row in df.iterrows()]
+        elif file_extension == 'txt':
+            txt_data = io.StringIO(input_file.read().decode('utf-8'))
+            lines = txt_data.readlines()
+        elif file_extension == 'docx':
+            doc = Document(input_file)
+            lines = [paragraph.text for paragraph in doc.paragraphs if paragraph.text.strip()]
+        else:
+            return 'Unsupported file format', 400
+
+        processed_data = process_data(lines)
         session['processed_data'] = processed_data  # Save the processed data in the session
 
         # Render output based on the chosen format
@@ -34,17 +57,17 @@ def index():
     return render_template('index.html')
 
 # Function to process input data
-def process_data(df):
+def process_data(lines):
     processed_data = []
 
-    for index, row in df.iterrows():
-        # Concatenate the first three columns of data regardless of name
-        raw_data = f"{row[df.columns[0]]}{row[df.columns[1]]}{row[df.columns[2]]}"
+    for index, line in enumerate(lines):
+        # Use the line as the raw data
+        raw_data = line.strip()
 
         # Call the ChatGPT API to process the raw data
         response = openai.Completion.create(
             engine="text-davinci-003",
-            prompt=f"Given the laboratory sample information: \"{raw_data}\", please separate the sample type, liquid class, and volume with commas. do not include labels such as sample type or use any symbols other than commas in the response",
+            prompt=f"Given the laboratory sample information: \"{raw_data}\", separate the sample type, liquid class, and volume with commas. do not include labels such as sample type or use any symbols other than commas in the response",
             max_tokens=50,
             n=1,
             stop=None,
